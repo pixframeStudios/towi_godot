363182d11e8b44636e4b8a6fabd34c53c0ef874b
Raúl Sánchez <64505983+Shigelo@users.noreply.github.com>
2022-10-18 19:01:00 -0500

    Update TowiPlugin

--- towi_godot/Runtime/TowiGameManager.gd
+++ towi_godot/Runtime/TowiGameManager.gd
@@ -2,25 +2,53 @@ extends Node
 
 var instance
 
+#Header
 var gameName = "Name of the game in towi db";
 var rounds = 3
 var pointsPerRound = 1;
 var score;
 var historyIndex = 0;
 
-var initialText 
+##
+var initialText
 var tutorialText;
-var confrimationText;
+var confirmationText;
 var positiveRoundResultText;
 var negativeRoundResultText;
 var finalPositiveResultText;
 var finaNegativelResultText;
+var bonusText;
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Init() # Replace with function body.
+
+func ShowInitialText():
+	pass
+
+##HELPERS
+func SceneInitialization():
+	#Useful for change StartMenu to InitialText
+	pass
+func GameInitialization():
+	#Useful for change from InitialText to tutorial text
+	pass
+func ExplaiGame():
+	#Useful for showing the tutorial text and images
+	pass
+func SetGameObjectives():
+	#Useful for setting up the rounds, objectives and bonus
+	pass
+func BeginGame():
+	#Useful for setting up the things that will appear on the game scene, and amking the game loop
+	pass
+func ShowResult():
+	#Useful for check and verify the score, the actualRound, the levels and showing it on the next screen
+	pass
+##END HELPERS
+
 
 func Init():
 	if(instance == null):
@@ -31,3 +59,4 @@ func Init():
 func OnDestroy():
 	if(instance == self):
 		instance = null
+		
